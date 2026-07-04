4c3d220bb0cad4c30f2c848d1d7e5b165e16c978
FrankJanePartner <partnermarvel55@gmail.com>
2026-07-04 01:17:10 +0100

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
 import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "grandroyaletrade.settings"
 )
 
-from django.core.wsgi import get_wsgi_application
-
-app = get_wsgi_application()
+application = get_wsgi_application()
+app = application
